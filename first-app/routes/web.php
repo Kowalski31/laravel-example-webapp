f3cbc2a0acc3ab5b8dde7929a52efffaeed7500e
Toan Lai <toan.lai@s3corp.com.vn>
2024-05-30 17:25:30 +0700

    add some routes

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/khanhtoan', function () {
+    return view('hi');
+});
+
+Route::get('/home', function (){
+    return view('home');
+});
